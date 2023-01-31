8265f273b6bf0be583683d17ecec369c3270cedf
nxxqxxj <joaquin7ap@gmail.com>
2023-01-31 14:22:17 -0300

    adding RG and APPSP to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,26 @@ required_providers {
 provider "azurerm" {
   features {}
 }
+
+##########  RESOURCE GROUP   ##########
+resource "azurerm_resource_group" "RG-RNDSXNG" {
+  name     = "RG-RNDSXNG"
+  location = "East US"
+  tags = {
+    "app"   = "RND-SXNG"
+    "proy"  = "RND-SXNG_AZ-TF"
+    "by"    = "nxxqxxj"
+  }
+}
+
+##########  APP SERVICE PLAN   ##########
+resource "azurerm_app_service_plan" "APPSP-RNDSXNG" {
+  name                = "APPSP-RNDSXNG"
+  location            = azurerm_resource_group.RG-RNDSXNG.location
+  resource_group_name = azurerm_resource_group.RG-RNDSXNG.name
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
