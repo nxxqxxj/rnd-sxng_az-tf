54b92b533bb1e0793eae27a52fafd55bb1f868d6
nxxqxxj <joaquin7ap@gmail.com>
2023-02-01 08:50:11 -0300

    changes in terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,7 +37,7 @@ resource "azurerm_linux_web_app" "APP" {
   name                = "APP-${var.prefix}"
   location            = azurerm_resource_group.RG.location
   resource_group_name = azurerm_resource_group.RG.name
-  service_plan_id = azurerm_app_service_plan.APPSP.id
+  service_plan_id = azurerm_service_plan.APPSP.id
 
   app_settings = {
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
