bb0ac86b6fceed82b1c6949796e9f46cd27dd843
nxxqxxj <joaquin7ap@gmail.com>
2023-02-21 11:08:35 -0300

    adding html and css to main page

--- src/App.js
+++ src/App.js
@@ -18,6 +18,24 @@ function App() {
         allow="encrypted-media"
       ></iframe>
       <button className="main-btn"> GET A SONG </button>
+      <div>
+        <button id="btnprevioussong" class="previous-btn">
+          ↩
+        </button>
+        <button id="btnuseraccess" class="second-btn">
+          GRANT PERMISSIONS
+        </button>
+        <button id="btnaddtoplaylist" class="second-btn" hidden>
+          ADD TO PLAYLIST
+        </button>
+      </div>
+      <a
+        href="https://linkedin.com/in/joaquin7ap"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <h6> © 2023 \\ NXXQXXJ</h6>
+      </a>
     </div>
   );
 }
